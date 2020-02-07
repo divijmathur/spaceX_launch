bdc28ad79f5bf5e44c7d62808986c2855440497c
Mona Lisa <divijmathur@gmail.com>
2020-02-06 21:34:15 -0800

    added launch details

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ const axios = require('axios');
 const graphqlHTTP = require('express-graphql');
 const schema = require('./schema');
 const app = express();
+const cors = require('cors');
+
+app.use(cors());
 app.use('/graphql',graphqlHTTP({
     schema,
     graphiql: true
