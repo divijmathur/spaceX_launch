85f2a481b0d0d9a68fd912e5aed8b44731cbc54f
Mona Lisa <divijmathur@gmail.com>
2020-02-12 15:57:33 -0800

    deploying to heroku

---
+++ public/precache-manifest.59fa349d0d2cec6473ad36200261a83b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee88f62a2c3d324f6a556761f8eca025",
+    "url": "/index.html"
+  },
+  {
+    "revision": "641081bbee5e05dc5df0",
+    "url": "/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "2564afdf46afb014765f",
+    "url": "/static/js/2.70d11454.chunk.js"
+  },
+  {
+    "revision": "a0c18f8786874a74d6fd4d46260562f6",
+    "url": "/static/js/2.70d11454.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "641081bbee5e05dc5df0",
+    "url": "/static/js/main.6b1129db.chunk.js"
+  },
+  {
+    "revision": "5d93c6b2d15332364552",
+    "url": "/static/js/runtime-main.cea588d5.js"
+  },
+  {
+    "revision": "7dab9996f15513443ab7ec4d1c04cab9",
+    "url": "/static/media/logo.7dab9996.jpg"
+  }
+]);
--- server.js
+++ server.js
@@ -4,12 +4,20 @@ const graphqlHTTP = require('express-graphql');
 const schema = require('./schema');
 const app = express();
 const cors = require('cors');
+const path = require('path');
+
 
 app.use(cors());
 app.use('/graphql',graphqlHTTP({
     schema,
     graphiql: true
 }));
+
+app.use(express.static('public'));
+
+app.get('*',(req,res) => {
+    res.sendFile(path.resolve(__dirname,'public','index.html'));
+});
 
 const PORT = process.env.PORT || 5000;
 
